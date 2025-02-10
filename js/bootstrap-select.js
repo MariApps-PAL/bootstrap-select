efde2b3eeded9312b5d49afd2bec88a6adb99d15
Sujith Sreedhar <sujith84@gmail.com>
2025-02-10 16:23:58 +0530

    Changed custom version to 15.0.1

--- js/bootstrap-select.js
+++ js/bootstrap-select.js
@@ -906,7 +906,7 @@
     this.init();
   };
 
-  Selectpicker.VERSION = 'M1';
+  Selectpicker.VERSION = '15.0.1';
 
   // part of this is duplicated in i18n/defaults-en_US.js. Make sure to update both.
   Selectpicker.DEFAULTS = {
